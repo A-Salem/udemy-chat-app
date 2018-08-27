bf1e49e63bf0c0eb795b180a870deb690a32030b
Ahmed Salem <a.salem@arabhosters.com>
2018-08-27 22:07:00 +0200

    Add Users class adn updateUserList event

---
+++ server/utils/validation.test.js
@@ -0,0 +1,20 @@
+const expect = require('expect');
+
+const {isRealString} = require('./validation');
+
+describe('isRealString', () => {
+  it('should reject non-string values', () => {
+    let res = isRealString(98);
+    expect(res).toBeFalsy();
+  });
+
+  it('should reject string with only spaces', () => {
+    let res = isRealString('     ');
+    expect(res).toBeFalsy();
+  });
+
+  it('should allow string with non-space characters', () => {
+    let res = isRealString('  Ahmed  ');
+    expect(res).toBe(true);
+  });
+});
